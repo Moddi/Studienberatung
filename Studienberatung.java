3984369a3819febb928b09b269cd14ab07c32609
Sascha Morlok <modgamers@hotmail.de>
2015-11-04 15:44:28 +0100

    Kleiner Fix

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-
 public class Studienberatung {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Hast du Abitur? [J/N]");
-		
+
 		String a = scanner.next();
 	}
-
 }
