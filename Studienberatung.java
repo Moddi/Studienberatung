3d204a532e20ebbd14d93b87269bcd048ae74b4a
Sascha Morlok <modgamers@hotmail.de>
2015-11-04 17:17:39 +0100

    Wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -4,9 +4,22 @@ public class Studienberatung {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
+		
 		System.out.println("Hast du Abitur? [J/N]");
 
 		String a = scanner.next();
+		
+		if (a.equalsIgnoreCase("j")) {
+			System.out.println("Bist du wissbegierig? [J/N]");
+			if (a.equalsIgnoreCase("j")) {
+				System.out.println("Auf jeden Fall studieren");
+			}
+			else {
+				System.out.println("Nicht studieren.");
+			}
+		}
+		else {
+			
+		}
 	}
 }
