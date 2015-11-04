e1d787ece19e6948c59ca8e386b839afde86b1bf
Sascha Morlok <modgamers@hotmail.de>
2015-11-04 15:35:55 +0100

    Erste Frage

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,6 +5,10 @@ public class Studienberatung {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Hast du Abitur? [J/N]");
+		
+		String a = scanner.next();
 	}
 
 }
